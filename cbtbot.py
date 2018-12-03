c930041d00bb9320ff9e01e817567bcefe7d165a
mSorro <mxmwrk@gmail.com>
2018-12-03 13:51:01 +0300

    Improved stability

--- cbtbot.py
+++ cbtbot.py
@@ -41,28 +41,42 @@ credentials_file.close
 # print(bottoken)
 # print(chat_id)
 
-def checktickets(urllist, bot, noticket):
+def checktickets(urllist, bot):
     """Chek tickets by scrapping urls"""
     print("Start check")
     for url in urllist:
-            print("Checking", url)
-            browser.get(url)
-            #time.sleep(3)
-            try:
-                    text = browser.find_element_by_tag_name('h2').text
-            except Exception as e:
-                    notify(bot, str(e) + " " + url)
-                    #print("error")
-                    noticket = 1
-            else:
-                    if (text != "Билетов нет") : 
-                            notify(bot, "Билеты есть? " + url)
-                            noticket = 1
-                    elif (text == "Билетов нет") : 
-                            #notify(bot, text)
-                            noticket = 0
-    print("Check complete, noticket = ", noticket)
-    return noticket
+        noticketinurls = 0   
+        noticket = 0
+        msg = ''
+        try:
+                noticket, msg = checkticketurl(url)
+        except Exception as e:
+                print(str(e))
+        print("Check complete, notickets = ", noticket)
+        if noticket > 0 : 
+                noticketinurls +=noticket
+                notify(bot, msg)
+    return noticketinurls
+
+def checkticketurl(url):
+        print("Checking", url)
+        browser.get(url)
+        try:
+                text = browser.find_element_by_tag_name('h2').text
+        except Exception as e:
+                msg = str(e) + " " + url
+                print(msg)
+                noticket = 1
+        else:
+                if (text != "Билетов нет") : 
+                        msg = "Билеты есть? " + url
+                        print(msg)
+                        noticket = 1
+                elif (text == "Билетов нет") : 
+                        msg = ''
+                        noticket = 0
+        return noticket, msg
+
 
 def notify(bot, text):
     """Notify me"""
@@ -77,19 +91,21 @@ def main():
     # Start the Bot
     updater.start_polling()
 
-    while True :
-        noticket = 0
-        noticket = checktickets(urls, bot, noticket)
-        print(noticket)
-        if noticket > 0 : 
-                print("Sleep 600 sec")
+    while True:
+        noticketinurls = 0
+        try:
+                noticketinurls = checktickets(urls,bot)
+        except Exception as e:
+                print(str(e))
                 time.sleep(600)
-        else :
-                print("Sleep 60 sec")
-                time.sleep(60)
-
-
-
+        else:
+                if (noticketinurls > 0) : 
+                        print("Sleep 600 sec")
+                        time.sleep(600)
+                elif (noticketinurls == 0) : 
+                        print("Sleep 60 sec")
+                        time.sleep(60)
+        
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
