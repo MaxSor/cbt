465044b0afebd11c55ce7effd3ec8f69daca34b0
mSorro <mxmwrk@gmail.com>
2018-12-04 20:51:47 +0300

    minor

--- cbtbot.py
+++ cbtbot.py
@@ -121,7 +121,7 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
-    disablebrowser((browser, display)
+    disablebrowser(browser, display)
     print("End")
    
 
