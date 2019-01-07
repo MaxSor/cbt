58b3715a9b1c68b5ba70d859b09bd9630f46923f
mSorro <mxmwrk@gmail.com>
2019-01-07 13:30:45 +0300

    remove extra imports

--- cbtbot2.py
+++ cbtbot2.py
@@ -4,9 +4,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import queue
 import threading
